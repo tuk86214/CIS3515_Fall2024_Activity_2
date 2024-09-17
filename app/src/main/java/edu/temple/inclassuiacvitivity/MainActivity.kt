53a085022bbae6d9ebb9e545f64d189e7f1a49d6
RamandeepNahar <ramandeep.s.nahar@gmail.com>
2024-09-17 11:57:27 -0400

    - Added array of multiples of 5 from 5 to 100 - Created ArrayAdapter to display numbers in Spinner - Implemented Spinner onItemSelectedListener to adjust TextView text size based on selected number

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -13,14 +13,23 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
-        //val numberArray = Array Of Numbers
+        val numberArray = Array(20) { i -> (i + 1) * 5 }
 
         /* TODO Step 3: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, numberArray)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
 
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedSize = numberArray[position]
+                displayTextView.textSize = selectedSize.toFloat()
+            }
 
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
     }
 }
